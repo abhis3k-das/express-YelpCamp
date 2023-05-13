a58cfcfc545de41e5ac0261ea80b6f9cd778c95f
abhis3k <abhis3k.das@gmail.com>
2023-05-13 11:28:05 +0530

    mend

--- index.js
+++ index.js
@@ -156,7 +156,8 @@ app.use((err, req, res, next) => {
     res.render('errors', { errorList, status })
 })
 
-app.listen(3000, () => {
-    console.log('Connected to Port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Connected to Port ${port}`)
 })
 
